5038cbfadb0e13c6a306c49378a5733c19f06fd4
pandurijal <pandurijal96@gmail.com>
2019-08-17 00:25:04 +0700

    Updates

---
+++ precache-manifest.38b9fb61821ad40568fdba8b3e607eee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a66afd7499a66165c6cd74d0b2cd5987",
+    "url": "/image-detection/index.html"
+  },
+  {
+    "revision": "cab2aa04578dd8a91eac",
+    "url": "/image-detection/static/css/main.43bf0d7a.chunk.css"
+  },
+  {
+    "revision": "d2a77f663e005566ea9b",
+    "url": "/image-detection/static/js/2.e454cf09.chunk.js"
+  },
+  {
+    "revision": "cab2aa04578dd8a91eac",
+    "url": "/image-detection/static/js/main.d6ee871b.chunk.js"
+  },
+  {
+    "revision": "230b49c1ae165beee96d",
+    "url": "/image-detection/static/js/runtime~main.56f48104.js"
+  }
+]);
